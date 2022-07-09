f62eb228debe42d468e2ffca973ec15a7000ce70
nicko <ikar2021@hotmail.com>
2022-07-09 11:00:36 +0300

    Add model for images

--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -9,6 +9,24 @@ class Location(models.Model):
     description_long = models.TextField()
     lng = models.FloatField()
     lat = models.FloatField()
+    img_url = models.ForeignKey('Image',
+                                verbose_name='Картинка места',
+                                null=True,
+                                blank=True,
+                                related_name='img',
+                                on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.title
+
+
+class Image(models.Model):
+    title = models.CharField(max_length=200)
+    img_url = models.ImageField(
+        upload_to='places_images',
+        blank=True,
+        verbose_name='Путь к картинке'
+        )
 
     def __str__(self):
         return self.title
