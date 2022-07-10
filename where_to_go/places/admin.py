8ff4647a67fac25debce47fca69445349b125933
nicko <ikar2021@hotmail.com>
2022-07-10 23:09:30 +0300

    Add image preview

--- where_to_go/places/admin.py
+++ where_to_go/places/admin.py
@@ -1,13 +1,22 @@
 from django.contrib import admin
 from .models import Location, Image
+from django.utils.html import format_html
 
 
 class ImageAdminInline(admin.TabularInline):
     model = Image
     verbose_name = 'Фотография'
     verbose_name_plural = 'Фотографии'
-    fields = ['img_url', 'position']
+    readonly_fields = ['get_preview', ]
+    fields = ['img_url', 'get_preview', 'position']
     extra = 0
+
+    def get_preview(self, obj):
+        return format_html(
+            '<img src="{url}" "width="150" height="150"/>'.format(
+                url=obj.img_url.url,
+                )
+            )
 
 
 @admin.register(Location)
