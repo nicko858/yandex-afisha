391a7b43badbfbc484a23b8378e74748d2bc7016
nicko <ikar2021@hotmail.com>
2022-11-07 01:27:19 +0300

    Edit settings for deploy

--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -140,17 +140,17 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
 
-#SECURE_HSTS_SECONDS = os.environ.get('SECURE_HSTS_SECONDS')
-#SECURE_HSTS_INCLUDE_SUBDOMAINS = (
-#    os.environ.get('SECURE_HSTS_INCLUDE_SUBDOMAINS', 'False') == 'True'
-#    )
-#SECURE_SSL_REDIRECT = (
-#    os.environ.get('SECURE_SSL_REDIRECT', 'False') == 'True'
-#    )
-#SESSION_COOKIE_SECURE = (
-#    os.environ.get('SESSION_COOKIE_SECURE', 'False') == 'True'
-#    )
-#CSRF_COOKIE_SECURE = (os.environ.get('CSRF_COOKIE_SECURE', 'False') == 'True')
-#SECURE_HSTS_PRELOAD = (
-#    os.environ.get('SECURE_HSTS_PRELOAD', 'False') == 'True'
-#    )
+SECURE_HSTS_SECONDS = os.environ.get('SECURE_HSTS_SECONDS')
+SECURE_HSTS_INCLUDE_SUBDOMAINS = (
+    os.environ.get('SECURE_HSTS_INCLUDE_SUBDOMAINS', 'False') == 'True'
+    )
+SECURE_SSL_REDIRECT = (
+    os.environ.get('SECURE_SSL_REDIRECT', 'False') == 'True'
+    )
+SESSION_COOKIE_SECURE = (
+    os.environ.get('SESSION_COOKIE_SECURE', 'False') == 'True'
+    )
+CSRF_COOKIE_SECURE = (os.environ.get('CSRF_COOKIE_SECURE', 'False') == 'True')
+SECURE_HSTS_PRELOAD = (
+    os.environ.get('SECURE_HSTS_PRELOAD', 'False') == 'True'
+    )
