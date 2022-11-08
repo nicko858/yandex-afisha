5038a30e72f8cabab4fb24d480bbaa7c3d566a42
nicko <ikar2021@hotmail.com>
2022-11-09 00:50:07 +0300

    Add domain to allowed_hosts

--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG', 'False') == 'True')
 
-ALLOWED_HOSTS = ['127.0.0.1', ]
+ALLOWED_HOSTS = ['127.0.0.1', 'nicko858.pythonanywhere.com']
 
 
 # Application definition
